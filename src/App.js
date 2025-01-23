b2c51aa88d4565c2d4dfaa277252c3a3fdf0007b
Amos omohodion <amosomohodion@gmail.com>
2025-01-23 15:41:55 +0100

    new message

--- src/App.js
+++ src/App.js
@@ -29,6 +29,8 @@ import { refreshToken } from './utils/refreshToken'
 import ReceiptViewer from './pages/ReceiptViewer'
 import NewExpenseNotifications from './pages/test2'
 
+const HOST = window.location.host;
+
 function isTokenExpired(token) {
   if (!token) return true
 
